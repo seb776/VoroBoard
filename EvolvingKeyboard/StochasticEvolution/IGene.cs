8ea187cf1aa4dfff2af592c70e6514d207aa8454
Sebastien Maire <sebastien.maire@epitech.eu>
2016-10-24 11:13:47 +0200

    Fixed compiler error.

--- EvolvingKeyboard/StochasticEvolution/IGene.cs
+++ EvolvingKeyboard/StochasticEvolution/IGene.cs
@@ -9,7 +9,7 @@ namespace EvolvingKeyboard.StochasticEvolution
     public interface IGene
     {
         bool Equals(IGene gene);
-        static IGene GenerateRandom();
+        IGene GenerateRandom();
         void Mutate(float coef); // 0 to infinity : defines how strong the gene will change
     }
 }
--- EvolvingKeyboard/StochasticEvolution/Individual.cs
+++ EvolvingKeyboard/StochasticEvolution/Individual.cs
@@ -23,12 +23,12 @@ namespace EvolvingKeyboard.StochasticEvolution
             DNA = new List<GENE_TYPE>(dnaSize);
         }
 
-        public static List<GENE_TYPE> GenerateDNAWithoutDoublons(int size)
+        public static List<GENE_TYPE> GenerateDNAWithoutDoublons(int size, Func<object[], GENE_TYPE> generateGeneRoutine, params object[] routineParams)
         {
             var newDNA = new List<GENE_TYPE>(size);
             for (int i = 0; i < newDNA.Count; ++i)
             {
-                var newGene = (GENE_TYPE)IGene.GenerateRandom();
+                var newGene = (GENE_TYPE)generateGeneRoutine(routineParams);
                 if (newDNA.Find((gene) => { return gene.Equals(newGene); }) != null)
                     i--;
                 else
@@ -37,14 +37,30 @@ namespace EvolvingKeyboard.StochasticEvolution
             return newDNA;
         }
 
+        public void ApplyMutation(float geneMutationCoef)
+        {
+            ApplyMutation(-1, -1, geneMutationCoef);
+        }
         public void ApplyMutation(int minGeneChangeCount, int maxGeneChangeCount, float geneMutationCoef)
         {
-            var randomGenerator = new Random();
             System.Diagnostics.Debug.Assert(DNA != null);
-            int finalGeneChange = randomGenerator.Next(minGeneChangeCount, maxGeneChangeCount);
+            System.Diagnostics.Debug.Assert(maxGeneChangeCount >= DNA.Count);
+            HashSet<int> mutatedGenesIndexes = new HashSet<int>(); // To avoid mutation of the same genes
+
+            var randomGenerator = new Random();
+            int finalGeneChange = 0;
+            if (minGeneChangeCount == -1 || maxGeneChangeCount == -1)
+                finalGeneChange = DNA.Count;
+            else
+             finalGeneChange = randomGenerator.Next(minGeneChangeCount, maxGeneChangeCount);
             for (int geneChangeCount = 0; geneChangeCount < finalGeneChange; ++geneChangeCount)
             {
-                int changeIndex = randomGenerator.Next(0, DNA.Count);
+                int changeIndex = 0;
+                do
+                {
+                    changeIndex = randomGenerator.Next(0, DNA.Count);
+                } while (mutatedGenesIndexes.Contains(changeIndex));
+                mutatedGenesIndexes.Add(changeIndex);
                 DNA[changeIndex].Mutate(geneMutationCoef);
             }
         }
